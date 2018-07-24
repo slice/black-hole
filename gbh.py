5841e7bea1dfb082994a1364de8e1543a64c0a59
slice <ryaneft@gmail.com>
2018-07-24 13:16:28 -0700

    add singularity

---
+++ gbh.py
@@ -0,0 +1,129 @@
+import asyncio
+import logging
+import sys
+import aiohttp
+
+import aioxmpp
+import toml
+
+logging.basicConfig(level='INFO')
+log = logging.getLogger('gajimbo.black-hole')
+
+
+class GajimboBH:
+    def __init__(self, jid, password, *, loop, config={}):
+        self.loop = loop
+        self.session = aiohttp.ClientSession(loop=loop)
+        self.config = config
+        self.jid = aioxmpp.JID.fromstr(jid)
+        self.password = password
+        self.selectors = [
+            aioxmpp.structs.LanguageRange.fromstr('*'),
+        ]
+        self.client = aioxmpp.PresenceManagedClient(
+            self.jid,
+            aioxmpp.make_security_layer(password, no_verify=True),
+        )
+
+    async def _version_xso_handler(self, iq):
+        log.info('received iq request from %s', iq.from_)
+        iq = aioxmpp.version.xso.Query()
+        iq.name = 'gajimbo black hole'
+        iq.version = '0.0.0'
+        iq.os = sys.platform
+        return iq
+
+    async def _handle_message(self, msg):
+        log.info('>>> %s', msg)
+
+    def _register(self):
+        messages = self.client.summon(aioxmpp.dispatcher.SimpleMessageDispatcher)
+        messages.register_callback(aioxmpp.MessageType.CHAT, None, self._handle_message)
+
+        self.client.stream.register_iq_request_handler(
+            aioxmpp.IQType.GET,
+            aioxmpp.version.xso.Query,
+            self._version_xso_handler,
+        )
+
+    def _clean_content(self, content):
+        return content \
+            .replace('@everyone', '@\u200beveryone') \
+            .replace('@here', '@\u200bhere') \
+            .replace('<@', '<\u200b@')
+
+    async def _bridge(self, nick, content):
+        url = self.config['bridge']['webhook']
+        payload = {
+            'username': nick,
+            'content': self._clean_content(content),
+        }
+
+        try:
+            await self.session.post(url, json=payload)
+        except aiohttp.ClientError:
+            log.exception('failed to bridge content')
+
+    def _on_muc_message(self, msg, member, source, **kwargs):
+        nick = member.nick
+        content = msg.body.lookup(self.selectors)
+
+        self.loop.create_task(self._bridge(nick, content))
+
+    def _join_mucs(self):
+        muc = self.client.summon(aioxmpp.MUCClient)
+
+        muc_config = self.config.get('muc', {})
+        nick = muc_config.get('nick', 'gajimbo')
+        room_names = muc_config.get('rooms', [])
+        server = muc_config.get('server')
+
+        if not server or not room_names:
+            log.warn('not connecting to mucs, not setup')
+            return
+
+        for room_name in room_names:
+            log.info('joining %s at %s', room_name, server)
+            room, fut = muc.join(
+                aioxmpp.JID.fromstr(
+                    f'{room_name}@{server}'
+                ),
+                nick,
+                history=aioxmpp.muc.xso.History(maxstanzas=0),
+            )
+            room.on_message.connect(self._on_muc_message)
+
+    def _set_presence(self):
+        self.client.set_presence(
+            aioxmpp.PresenceState(available=True, show=aioxmpp.PresenceShow.CHAT),
+            'absorbing matter',
+        )
+
+    async def _run(self):
+        async with self.client.connected() as stream:
+            log.info('obtained stream: %s', stream)
+            self._register()
+            self._set_presence()
+            self._join_mucs()
+
+            while True:
+                await asyncio.sleep(60)
+
+    def run(self):
+        self.loop.run_until_complete(self._run())
+
+
+if __name__ == '__main__':
+    config = toml.load('config.toml')
+    jid = config['xmpp']['jid']
+
+    log.info('using jid: %s', jid)
+    log.info('connecting...')
+
+    gbh = GajimboBH(
+        jid,
+        config['xmpp']['password'],
+        loop=asyncio.get_event_loop(),
+        config=config,
+    )
+    gbh.run()
